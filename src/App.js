b99d56e8db20cac01a26c1ca164b9917b20d0f81
evelyn-2022 <evelyn20220101@gmail.com>
2022-09-08 00:42:13 +0800

    refactered code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useReducer } from 'react';
 import { DigitButton } from './DigitButton';
 import { OperationButton } from './OperationButton';
+import { reducer } from './reducer';
+import { formatOperand } from './formatOperand';
 
 export const ACTIONS = {
   ADD_DIGIT: 'add-digit',
@@ -8,115 +10,6 @@ export const ACTIONS = {
   CLEAR: 'clear',
   DELETE_DIGIT: 'delete digit',
   EVALUATE: 'evaluate',
-};
-
-const reducer = (state, { type, payload }) => {
-  switch (type) {
-    case ACTIONS.ADD_DIGIT:
-      if (state.overwrite) {
-        return { ...state, currentOperand: payload.digit, overwrite: false };
-      }
-
-      if (payload.digit === '0' && state.currentOperand === '0') return state;
-      if (payload.digit !== '0' && state.currentOperand === '0')
-        return { ...state, currentOperand: payload.digit };
-      if (payload.digit === '.' && state.currentOperand == null)
-        return { ...state, currentOperand: '0.' };
-      if (payload.digit === '.' && state.currentOperand.includes('.'))
-        return state;
-      return {
-        ...state,
-        currentOperand: `${state.currentOperand || ''}${payload.digit}`,
-      };
-
-    case ACTIONS.CHOOSE_OPERATION:
-      if (state.currentOperand == null && state.previousOperand == null) {
-        return state;
-      }
-
-      if (state.currentOperand == null) {
-        return { ...state, operation: payload.operation };
-      }
-
-      if (state.previousOperand == null) {
-        return {
-          ...state,
-          operation: payload.operation,
-          previousOperand: state.currentOperand,
-          currentOperand: null,
-        };
-      }
-      return {
-        ...state,
-        operation: payload.operation,
-        previousOperand: evaluate(state),
-        currentOperand: null,
-      };
-
-    case ACTIONS.CLEAR:
-      return {};
-
-    case ACTIONS.DELETE_DIGIT:
-      if (state.overwrite)
-        return { ...state, currentOperand: null, overwrite: false };
-      if (state.currentOperand == null) return state;
-      if (state.currentOperand.length === 1)
-        return { ...state, currentOperand: null };
-      return { ...state, currentOperand: state.currentOperand.slice(0, -1) };
-
-    case ACTIONS.EVALUATE:
-      if (
-        state.operation == null ||
-        state.currentOperand == null ||
-        state.previousOperand == null
-      ) {
-        return state;
-      }
-
-      return {
-        ...state,
-        overwrite: true,
-        operation: null,
-        previousOperand: null,
-        currentOperand: evaluate(state),
-      };
-  }
-};
-
-const evaluate = ({ currentOperand, previousOperand, operation }) => {
-  const prev = parseFloat(previousOperand);
-  const current = parseFloat(currentOperand);
-
-  if (isNaN(prev) || isNaN(current)) return '';
-
-  let computation = '';
-
-  switch (operation) {
-    case '+':
-      computation = prev + current;
-      break;
-    case '-':
-      computation = prev - current;
-      break;
-    case '×':
-      computation = prev * current;
-      break;
-    case '÷':
-      computation = prev / current;
-  }
-
-  return computation.toString();
-};
-
-const INTEGER_FORMATTER = new Intl.NumberFormat('en-us', {
-  maximumFractionDigits: 0,
-});
-
-const formatOperand = operand => {
-  if (!operand) return;
-  const [integer, decimal] = operand.split('.');
-  if (decimal == null) return INTEGER_FORMATTER.format(integer);
-  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`;
 };
 
 const App = () => {
@@ -131,8 +24,6 @@ const App = () => {
     reducer,
     {}
   );
-
-  // dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } });
 
   return (
     <main>
